540bb8a516f003546782a700142083cc7d454cf2
PRATHAM KARMARKAR <karmarkarpratham56@gmail.com>
2024-07-06 09:49:56 +0530

    navigate package enter

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,9 +4,13 @@ import { useNavigate } from 'react-router-dom';
 const Footer = () => {
   const navigate = useNavigate();
 
-  const handlePrivacyPolicyClick = () => {
+  const handlePrivacyPolicyClick3 = () => {
     console.log("hello");
     navigate('/Peepee');
+  };
+  const handlePrivacyPolicyClick2 = () => {
+    console.log("hello");
+    navigate('/Terms');
   };
 
   return (
@@ -14,8 +18,8 @@ const Footer = () => {
       <footer className="page-footer">
         <div className="footer-list">
           <button className="list-item">CONTACT US</button>
-          <button className="list-item">TERMS OF SERVICE</button>
-          <button className="list-item" onClick={handlePrivacyPolicyClick}>PRIVACY POLICY</button>
+          <button className="list-item" onClick={handlePrivacyPolicyClick2}>TERMS OF SERVICE</button>
+          <button className="list-item" onClick={handlePrivacyPolicyClick3}>PRIVACY POLICY</button>
         </div>
       </footer>
     </div>
