1d076d62d035dc8dee94534633edd819b5815844
anmol420 <anmolanand2k15@gmail.com>
2024-12-11 00:22:38 +0530

    Added User Routes

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,47 +1,97 @@
-import  asyncHanlder from "../utils/asyncHandler.js";
-import  ApiError  from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError  from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
-import  ApiResponse  from "../utils/ApiResponse.js";
+import ApiResponse  from "../utils/ApiResponse.js";
+import { COOKIE_OPTIONS } from "../constants.js";
+import jwt from "jsonwebtoken";
 
-
-const registerUser = asyncHanlder( async (req,res)=>{
-    const { username , email , password , age , name } = req.body;
-
-    if (
-        [username,email,password].some((field)=>
-            field?.trim()===""
-        )
-    ) {
-        throw new ApiError(400,"all fields are required")
+const registerUser = asyncHandler( async (req,res) => {
+    const { username, email, password } = req.body;
+    if (!(username || email || password)) {
+        throw new ApiError(400, "All Fields Are Required.")
     }
 
     const existedUser = await User.findOne({
-        $or: [{ username },{ email }]
-    })
-
-    if(existedUser){
-        throw new ApiError(409,"User already exists")
+        $or: [{ username }, { email }]
+    });
+    if (existedUser) {
+        throw new ApiError(409, "User Already Exists.")
     }
 
-    const user = await User.create({
-        email,
-        username,
-        name,
-        password,
-        age
-    })
+    try {
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+        return res
+            .status(201)
+            .json(new ApiResponse(201, user, "User Created Successfully."));
+    } catch (error) {
+        throw new ApiError(500, error.message || "Internal Server Error.");
+    }
+});
 
-    const createdUser = await User.findById(user._id).select(
-        "-password"
-    )
+const generateToken = async (user) => {
+    try {
+        const token = await jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRY,
+            }
+        );
+        return token;
+    } catch (error) {
+        throw new ApiError(500, error.message || "Internal Server Error.");
+    }
+};
 
-    if(!createdUser){
-        throw new ApiError(500,"Internal Sever error")
+const loginUser = asyncHandler( async (req,res) => {
+    const { username, password } = req.body;
+    if (!(username || password)) {
+        throw new ApiError(400, "All Fields Are Required.")
     }
 
-    return res.status(201).json(
-        new ApiResponse(200,createdUser,"User creation success")
-    )
-})
+    const user = await User.findOne({ username });
+    if (!user) {
+        throw new ApiError(404, "User Not Found.")
+    }
 
-export { registerUser }
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Invalid Credentials.")
+    }
+
+    const token = await generateToken(user);
+    if (!token) {
+        throw new ApiError(500, "Token Generation Failed.")
+    }
+    try {
+        return res
+            .cookie("token", token, COOKIE_OPTIONS)
+            .status(200)
+            .json(new ApiResponse(200, null, "User Logged In Successfully."));
+    } catch (error) {
+        throw new ApiError(500, error.message || "Internal Server Error.");
+    }
+});
+
+const logoutUser = asyncHandler( async (req,res) => {
+    try {
+        return res
+            .clearCookie("token")
+            .status(200)
+            .json(new ApiResponse(200, null, "User Logged Out Successfully."));
+    } catch (error) {
+        throw new ApiError(500, error.message || "Internal Server Error.");
+    }
+});
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+};
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { registerUserSchema, loginUserSchema } from "../utils/zodSchema/userValidatorSchema.js";
+import validationSchema from "../middlewares/zodValidator.middleware.js";
+import verifyToken from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(validationSchema(registerUserSchema), registerUser);
+router.route("/login").post(validationSchema(loginUserSchema), loginUser);
+
+// secured routes
+router.route("/logout").post(verifyToken, logoutUser);
 
 export default router;
