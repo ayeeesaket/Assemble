e020d472ea55b293d2a1088647653c806929c131
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-12-19 14:50:35 +0530

    Minor Fixes

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -39,14 +39,14 @@ router.route("/verifyCode").post(validationSchema(verifyCodeSchema), verifyCode)
 router.route("/forgotUsername").post(validationSchema(forgotUsernameSchema), forgotUsername);
 router.route("/forgotPasswordVerificationEmail").post(validationSchema(forgotPasswordVerificationEmailSchema), forgotPasswordVerificationEmail);
 router.route("/forgotPasswordVerificationCode").post(validationSchema(forgotPasswordVerificationCodeSchema), forgotPasswordVerificationCode);
-router.route("/forgotPassword").patch(validationSchema(forgotPasswordSchema), forgotPassword);//change to patch
+router.route("/forgotPassword").patch(validationSchema(forgotPasswordSchema), forgotPassword);
 
 // secured routes
 router.route("/logout").post(verifyToken, logoutUser);
-router.route("/changeUsername").patch(verifyToken, validationSchema(changeUsernameSchema), changeUsername);//change to patch
-router.route("/changePassword").patch(verifyToken, validationSchema(changePasswordSchema), changePassword);//change to patch
+router.route("/changeUsername").patch(verifyToken, validationSchema(changeUsernameSchema), changeUsername);
+router.route("/changePassword").patch(verifyToken, validationSchema(changePasswordSchema), changePassword);
 router.route("/addDetails").post(verifyToken, validationSchema(addDetailsSchema), addDetails);
-router.route("/changeEmail").patch(verifyToken, validationSchema(changeEmailSchema), changeEmail);//change to patch
+router.route("/changeEmail").patch(verifyToken, validationSchema(changeEmailSchema), changeEmail);
 router.route("/verifyNewEmail").post(verifyToken, validationSchema(verifyNewEmailSchema), verifyNewEmail);
 
 export default router;
